d64e433c9f2438a4cdd6f6d401558d8a3e4e8831
alex-avila <alex.avilx@icloud.com>
2018-07-04 00:38:58 -0600

    complete most independent presentational parts of the portfolio.

--- index.js
+++ index.js
@@ -3,25 +3,49 @@ Vue.component('intro', {
         <div class="intro utility-wrapper">
             <div class="intro__background"></div>
             <div class="intro__nav">
-                <span>Alex Avila</span>
-                <span>Menu</span>
+                <span class="intro__nav__logo">Alex Avila</span>
+                <span class="intro__nav__menu"></span>
             </div>
             <div class="intro__headline">
-                <h1>I'm a full stack developer with an affinity for design.</h1>
+                <span>I'm a full stack developer with an affinity for design.</span>
             </div>
             <div class="intro__skills">
-                <h3>Main Skills</h3>
+                <h4>Main Skills</h4>
                 <p>React, Redux, ES6, MongoDB, Express, Node JS, Vue, React Native</p>
             </div>
+            <div class="intro__scroll-btn"></div>
         </div>
     `
 })
 
 Vue.component('projects', {
     template: `
-        <div class="utility-wrapper">
-            <h3>Projects</h3>
+        <div class="projects utility-wrapper">
+            <h3 class="section-title projects__title">Projects</h3>
         </div>
+    `
+})
+
+Vue.component('connect', {
+    template: `
+        <div class="connect utility-wrapper">
+            <h3 class="section-title connect__title">Connect</h3>
+            <div class="connect__content">
+                <p>Thank you for visiting my portfolio.</p>
+                <p>You may contact me to discuss career opportunities (or anything really) at alex.avilx@gmail.com</p>
+            </div>
+            <div class="connect__icons">
+                <span class="icon github"></span>
+                <span class="icon dribbble"></span>
+                <span class="icon linkedin"></span>
+            </div>
+        </div>
+    `
+})
+
+Vue.component('vfooter', {
+    template: `
+        <div class="footer">Copyright &copy; 2018 blahblahblah</div>
     `
 })
 
