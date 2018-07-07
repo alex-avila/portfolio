b7d999f24027e1b11074fc72d8582073cd9afd13
alex-avila <alex.avilx@icloud.com>
2018-07-06 23:30:59 -0600

    create basic portfolio with vue cli

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="footer">Copyright &copy; 2018 blahblahblah</div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
---
+++ src/components/Projects.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="projects utility-wrapper">
+        <h3 class="section-title projects__title">Projects</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Projects"
+};
+</script>
