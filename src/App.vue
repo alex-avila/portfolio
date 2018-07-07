d622de76e118d8ce3b2342938eb4b0be044d8eec
alex-avila <alex.avilx@icloud.com>
2018-07-06 23:50:09 -0600

    removed unnecessary comment

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Intro />
     <Projects />
     <Connect />
@@ -50,93 +48,93 @@ body {
 }
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 html,
 body,
 .wrapper {
-    margin: 0;
-    font-size: 18px;
-    font-family: 'Avenir';
-    height: 100%;
-    background: #FCFDFF;
-    color: #24195F;
-    line-height: 1.618;
+  margin: 0;
+  font-size: 18px;
+  font-family: "Avenir";
+  height: 100%;
+  background: #fcfdff;
+  color: #24195f;
+  line-height: 1.618;
 }
 
 h4 {
-    font-size: 24px;
-    font-weight: 500;
+  font-size: 24px;
+  font-weight: 500;
 }
 
 h3 {
-    font-size: 30px;
+  font-size: 30px;
 }
 
 .section-title {
-    color: #202ED5;
+  color: #202ed5;
 }
 
 .section-title::after {
-    content: "";
-    display: block;
-    background: #FDCEFF;
-    height: 7px;
-    width: 66px;
-    margin: -15px 0 0 -1px;
+  content: "";
+  display: block;
+  background: #fdceff;
+  height: 7px;
+  width: 66px;
+  margin: -15px 0 0 -1px;
 }
 
 .connect {
-    padding-bottom: 3em;
-    line-height: 2;
+  padding-bottom: 3em;
+  line-height: 2;
 }
 
 .connect__title {
-    line-height: 1.618
+  line-height: 1.618;
 }
 
 .connect__content {
-    text-align: center;
-    padding: 1.618em 0 1.618em;
+  text-align: center;
+  padding: 1.618em 0 1.618em;
 }
 
 .connect__content p:first-child {
-    padding: 0.618em;
+  padding: 0.618em;
 }
 
 .connect__icons {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    padding: 3em 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 3em 0;
 }
 
 .github {
-    background: url(./assets/github.svg)
+  background: url(./assets/github.svg);
 }
 
 .dribbble {
-    background: url(./assets/dribbble.svg)
+  background: url(./assets/dribbble.svg);
 }
 
 .linkedin {
-    background: url(./assets/linkedin.svg)
+  background: url(./assets/linkedin.svg);
 }
 
 .icon {
-    display: block;
-    width: 45px;
-    height: 45px;
-    background-repeat: no-repeat;
-    background-size: contain;
-    cursor: pointer;
+  display: block;
+  width: 45px;
+  height: 45px;
+  background-repeat: no-repeat;
+  background-size: contain;
+  cursor: pointer;
 }
 
 .footer {
-    display: flex;
-    justify-content: center;
-    padding: 0 0 3em;
-    font-size: 15px;
+  display: flex;
+  justify-content: center;
+  padding: 0 0 3em;
+  font-size: 15px;
 }
 </style>
