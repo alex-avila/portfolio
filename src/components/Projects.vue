ea6a77193129abf338de84ac6624621776d0df3a
alex-avila <alex.avilx@icloud.com>
2018-07-12 20:04:00 -0600

    add toggle of development and ux in projects section

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="projects" class="utility-wrapper projects">
-        <h3 class="section-title projects__title">Projects</h3>
+        <div class="projects__title-and-toggle">
+            <h3 class="section-title projects__title">Projects</h3>
+            <ProjectsToggle :selectOption="selectOption" :optionSelected="optionSelected"/>
+        </div>
         <div class="projects__projects">
             <slot v-for="project in projects">
                 <Project
@@ -16,56 +19,75 @@
 </template>
 
 <script>
-import Project from './Project.vue'
+import Project from "./Project.vue";
+import ProjectsToggle from "./ProjectsToggle.vue";
 
 export default {
   name: "Projects",
   components: {
-    Project
+    Project,
+    ProjectsToggle
   },
   data: function() {
-      return {
-          projects: [
-              {
-                image: require('../assets/re-act-arcade.jpg'),
-                link: 'https://re-act-arcade.herokuapp.com/',
-                title: '(re)act arcade',
-                description: 'Arcade group project. I contributed on the backend, frontend, UI, and the database structure.'
-              },
-              {
-                image: require('../assets/wario-srs.jpg'),
-                link: 'https://wario-srs.herokuapp.com/',
-                title: 'Wario SRS',
-                description: 'Simple personal flashcard app that uses the SuperMemo spaced repetition algorithm. (Work in progress).'
-              },
-              {
-                image: require('../assets/tic-tac-toe.jpg'),
-                link: 'https://alex-avila.github.io/tic-tac-toe-world/#/',
-                title: 'Tic Tac Toe World',
-                description: 'A.I. that creates an unbeatable tic tac toe game. Displays weather data from an API.'
-              },
-              {
-                image: require('../assets/memory-game.jpg'),
-                link: 'https://alex-avila.github.io/memory-game/',
-                title: 'RPG Memory Game',
-                description: 'Simple game of concentration in React.'
-              },
-          ]
-      }
+    return {
+      optionSelected: 0,
+      projects: [
+        {
+          image: require("../assets/re-act-arcade.jpg"),
+          link: "https://re-act-arcade.herokuapp.com/",
+          title: "(re)act arcade",
+          description:
+            "Arcade group project. I contributed on the backend, frontend, UI, and the database structure."
+        },
+        {
+          image: require("../assets/wario-srs.jpg"),
+          link: "https://wario-srs.herokuapp.com/",
+          title: "Wario SRS",
+          description:
+            "Simple personal flashcard app that uses the SuperMemo spaced repetition algorithm. (Work in progress)."
+        },
+        {
+          image: require("../assets/tic-tac-toe.jpg"),
+          link: "https://alex-avila.github.io/tic-tac-toe-world/#/",
+          title: "Tic Tac Toe World",
+          description:
+            "A.I. that creates an unbeatable tic tac toe game. Displays weather data from an API."
+        },
+        {
+          image: require("../assets/memory-game.jpg"),
+          link: "https://alex-avila.github.io/memory-game/",
+          title: "RPG Memory Game",
+          description: "Simple game of concentration in React."
+        }
+      ]
+    };
   },
   props: {
     isModeSmall: Boolean
+  },
+  methods: {
+    selectOption: function(selection) {
+      this.optionSelected = selection;
+    }
   }
 };
 </script>
 
 <style lang="sass" scoped>
 #projects
-    padding-top: 3em;
+    padding-top: 3em
+
+.projects__title-and-toggle
+    display: flex
+    flex-direction: column
+    @media screen and (min-width: 650px)
+        flex-direction: row
+        justify-content: space-between
+        align-items: center;
 
 .projects__projects
-    display: grid;
-    gap: 10px;
-    grid-template-columns: repeat(auto-fit, minmax(290px, 1fr));
+    display: grid
+    gap: 10px
+    grid-template-columns: repeat(auto-fit, minmax(290px, 1fr))
 </style>
 
---
+++ src/components/ProjectsToggle.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="toggle">
+        <div id="development" class="toggle__option toggle__option-1" :class="{selected: optionSelected === 0}" v-on:click="selectOption(0)">
+            <span>Development</span>
+        </div>
+        <div id="ux-design" class="toggle__option toggle__option-2" :class="{selected: optionSelected === 1}" v-on:click="selectOption(1)">
+            <span>UX Design</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectsToggle",
+  props: {
+      selectOption: Function,
+      optionSelected: Number
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.toggle {
+  display: inline-grid;
+  grid-template-columns: 1fr 1fr;
+  border-radius: 9px;
+  overflow: hidden;
+  margin: auto;
+  font-weight: 500;
+  @media screen and (min-width: 650px) {
+    margin: 0;
+  }
+}
+
+.toggle__option {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.1em 0.618em;
+  background: #fcfdff;
+  color: #532fec;
+  border: 1.5px solid #532fec;
+  font-size: 16px;
+  cursor: pointer;
+  transition: all 0.25s;
+}
+
+.toggle__option span {
+  margin-top: 1.5px;
+}
+
+.toggle__option-1 {
+  border-radius: 9px 0 0 9px;
+  border-right: 1px solid #532fec;
+}
+
+.toggle__option-2 {
+  border-radius: 0 9px 9px 0;
+}
+
+.selected {
+  background: #532fec;
+  color: #fcfdff;
+}
+</style>
+
+
